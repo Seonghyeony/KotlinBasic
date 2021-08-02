b69884828f6761595499afa064fd45c66e0b71b0
Seonghyeony <sunghyun7949@naver.com>
2021-08-02 23:09:20 +0900

    feat: dataType

---
+++ DataType/src/main/kotlin/DataType.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var username = "seonghyeon"
+    val username2: String
+    username2 = "Test"
+    val count = 3
+    username = "im_seonghyeon"
+
+    val exp02 = 1_000_000
+    val exp03 = 0x0F
+    val exp04 = 0b00001011
+
+    println("username: $username, count: $count")
+    println("exp02 = $exp02, exp03 = $exp03, exp04 = $exp04")
+}
